f048557b4743af4c0607aae4b8063e9b4e212470
LI <2943753234@qq.com>
2021-11-13 23:12:33 +0800

    fix: fix findTopic

--- src/controller/utils.js
+++ src/controller/utils.js
@@ -9,50 +9,50 @@ async function findTopic(target, obj) {
   let res3
   let res4
   try {
-    res1 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights<='${target}' and category = '简答题' order by rand() limit ${jian};`, {
+    res1 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights<'${target}' and category = '简答题' order by rand() limit ${jian};`, {
       type: Sequelize.QueryTypes.SELECT
     })
     if (!res1) {
       res1 = []
     }
     if (res1.length < jian) {
-      const _res1 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights > '${target}' and category = '简答题' order by rand() limit ${jian - res1.length};`, {
+      const _res1 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights >= '${target}' and category = '简答题' order by rand() limit ${jian - res1.length};`, {
         type: Sequelize.QueryTypes.SELECT
       })
       res1 = res1.concat(_res1)
     }
-    res2 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights<='${target}' and category = '名词解释' order by rand() limit ${ming};`, {
+    res2 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights<'${target}' and category = '名词解释' order by rand() limit ${ming};`, {
       type: Sequelize.QueryTypes.SELECT
     })
     if (!res2) {
       res2 = []
     }
     if (res2.length < ming) {
-      const _res2 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights > '${target}' and category = '名词解释' order by rand() limit ${ming - res2.length};`, {
+      const _res2 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights >= '${target}' and category = '名词解释' order by rand() limit ${ming - res2.length};`, {
         type: Sequelize.QueryTypes.SELECT
       })
       res2 = res2.concat(_res2)
     }
-    res3 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where  weights<='${target}' and category = '论述题' order by rand() limit ${lun};`, {
+    res3 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where  weights<'${target}' and category = '论述题' order by rand() limit ${lun};`, {
       type: Sequelize.QueryTypes.SELECT
     })
     if (!res3) {
       res3 = []
     }
     if (res3.length < lun) {
-      const _res3 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights > '${target}' and category = '论述题' order by rand() limit ${lun - res3.length};`, {
+      const _res3 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights >= '${target}' and category = '论述题' order by rand() limit ${lun - res3.length};`, {
         type: Sequelize.QueryTypes.SELECT
       })
       res3 = res3.concat(_res3)
     }
-    res4 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where  weights<='${target}' and category = '综合题' order by rand() limit ${zong};`, {
+    res4 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where  weights< '${target}' and category = '综合题' order by rand() limit ${zong};`, {
       type: Sequelize.QueryTypes.SELECT
     })
     if (!res4) {
       res4 = []
     }
     if (res4.length < zong) {
-      const _res4 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights > '${target}' and category = '综合题' order by rand() limit ${zong - res4.length};`, {
+      const _res4 = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights >= '${target}' and category = '综合题' order by rand() limit ${zong - res4.length};`, {
         type: Sequelize.QueryTypes.SELECT
       })
       res4 = res4.concat(_res4)
@@ -69,14 +69,14 @@ async function findTopic(target, obj) {
 async function findFramework(target) {
   let res = []
   try {
-    res = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights<='${target}' and category = '框架' order by rand() limit 2;`, {
+    res = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights<'${target}' and category = '框架' order by rand() limit 2;`, {
       type: Sequelize.QueryTypes.SELECT
     })
     if (!res) {
       res = []
     }
     if (res.length < 2) {
-      const _res = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights > '${target}' and category = '框架' order by rand() limit ${2 - res.length};`, {
+      const _res = await db.seq.query(`select id, name,content,update_time,create_time, category,weights from topic where weights >= '${target}' and category = '框架' order by rand() limit ${2 - res.length};`, {
         type: Sequelize.QueryTypes.SELECT
       })
       res = res.concat(_res)
