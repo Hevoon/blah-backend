3c6abe9fb62ac85cfa2ba2f230d577d3cc86a93d
LI <2943753234@qq.com>
2023-02-25 00:57:21 +0800

    feat: 配置变更

--- src/configs/config.js
+++ src/configs/config.js
@@ -2,7 +2,7 @@ let config = {
   database: 'blah', // 使用哪个数据库
   username: 'root', // 用户名
   password: 'fuckmysql123', // 口令
-  host: '172.18.53.203', // 主机名
+  host: '47.97.158.44', // 主机名
   port: 3306, // 端口号，MySQL默认3306,
   dialect: 'mysql',
   pool: {
