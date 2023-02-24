3018a8db18f2f131c54a67abd16597fe57616053
LI <2943753234@qq.com>
2023-02-25 04:41:07 +0800

    fix: 更新mysql

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,8 +1,8 @@
 let config = {
   database: 'blah', // 使用哪个数据库
   username: 'root', // 用户名
-  password: 'fuckmysql123', // 口令
-  host: '47.97.158.44', // 主机名
+  password: 'root', // 口令47.97.158.44
+  host: 'localhost', // 主机名
   port: 3306, // 端口号，MySQL默认3306,
   dialect: 'mysql',
   pool: {
--- src/middleware/myDone/index.js
+++ src/middleware/myDone/index.js
@@ -3,14 +3,14 @@ const resTime = require('./resTime')();
 
 
 async function midd2(ctx, next) {
-    console.log('let us begin');
-    await next();
-    console.log(ctx.path);
+  console.log('let us begin');
+  await next();
+  console.log(ctx, ctx.path);
 }
 
 
 const all = compose([resTime, midd2]);
 
 module.exports = () => {
-    return all
+  return all
 };
