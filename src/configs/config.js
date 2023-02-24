12f9369ab5496e479d09d4b87886991c96cfcd3b
LI <2943753234@qq.com>
2023-02-24 23:36:15 +0800

    chore: 更新数据库实例地地址

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,15 +1,15 @@
 let config = {
-    database: 'blah', // 使用哪个数据库
-    username: 'root', // 用户名
-    password: '123456', // 口令
-    host: '101.132.132.215', // 主机名
-    port: 3306, // 端口号，MySQL默认3306,
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    }
+  database: 'blah', // 使用哪个数据库
+  username: 'root', // 用户名
+  password: 'fuckmysql123', // 口令
+  host: '172.18.53.203', // 主机名
+  port: 3306, // 端口号，MySQL默认3306,
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
 };
 
 module.exports = config;
